9c8cd8fe95558f4ebede072f29f115cab2a0252c
Tre'Shawn <nbatre1@yahoo.com>
2019-07-07 00:49:30 -0500

    keep moving forward

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,7 +7,7 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 
 // set Intial state of menu
